953c2c75b9b32c3879f8009798f90059a35bbf48
hingino <hingino@gmail.com>
2023-04-08 15:09:46 -0600

    make test more readable

--- main.go
+++ main.go
@@ -10,20 +10,23 @@ func main() {
 	gpus := hwinfo.GetGpuData()
 	isNotebook := hwinfo.GetChassisInfo()
 
-	println("TEST 1: Get info for installed gpus")
-	println("Is Notebook?: ", isNotebook)
+	println("TEST 1: check if device is notebook")
+	println("Is Notebook?: ", isNotebook, "\n")
+
+	println("TEST 2: Get metadata for all detected GPUs\n")
 	for i, gpu := range gpus {
+		fmt.Printf("TEST 2.%v: Get online metadata", i+1)
+
 		println("GPU Vendor: ", gpu.Vendor)
 		println("GPU Name: ", gpu.Name)
 		println("Driver: ", gpu.OfflineDriver)
 
-		fmt.Printf("TEST 2.%v: Get online metadata", i+1)
 		gpuId, err := metadata.GetGpuId(gpu.Vendor, gpu.Name, isNotebook)
 		if err != nil {
 			panic(err)
 		}
 		// deref_metadata := *metadata
-		println("GPU ID: ", gpuId)
+		println("GPU ID: ", gpuId, "\n")
 		// println(deref_metadata)
 	}
 
